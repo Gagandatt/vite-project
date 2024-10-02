62a9f5773ab9d0a217da4bf97d068b31f813ae05
Gagan Rewala <gaganrewala@Gagans-MacBook-Air.local>
2024-10-02 23:25:47 +0530

    first commit

---
+++ src/components/Post.tsx
@@ -0,0 +1,18 @@
+function Post() {
+  const products = [
+    { title: "Cabbage", id: 1 },
+    { title: "Garlic", id: 2 },
+    { title: "Apple", id: 3 },
+  ];
+  return (
+    <div className="text-center mt-10">
+      <ul className="text-4xl font-bold text-blue-500">
+        {products.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Post;
